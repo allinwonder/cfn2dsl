973434cf5b8ee83eecf92fa90000fb711cca9e06
Cam Maxwell <camm@amazon.com>
2019-08-11 02:45:38 +1000

    Added spec test for fix of #14

--- spec/intrinsic_function_spec.rb
+++ spec/intrinsic_function_spec.rb
@@ -17,6 +17,30 @@ RSpec.describe IntrinsicFunction do
     it_behaves_like 'an intrinsic function'
   end
 
+  context 'Fn::Sub(string)' do
+    let(:name) { 'Fn::Sub' }
+    let(:input) {'I am a ${single} string substitution'}
+    it "creates an intrinsic function with string" do
+      expect(subject.name).to eq(name)
+      expect(subject.parameters).to eq(input)
+      expect(subject.parameters).to be_instance_of String
+    end
+    it_behaves_like 'an intrinsic function'
+  end
+
+  context 'Fn::Sub(hash)' do
+    let(:name) { 'Fn::Sub' }
+    let(:input) { ['I am a ${hashkey} substitution', {'hashkey' => 'hashvalue'}] }
+    it "creates an intrinsic function with hash" do
+      expect(subject.name).to eq(name)
+      expect(subject.parameters).to eq(input)
+      expect(subject.parameters).to be_instance_of Array
+      expect(subject.parameters[0]).to be_instance_of String
+      expect(subject.parameters[1]).to be_instance_of Hash
+    end
+    it_behaves_like 'an intrinsic function'
+  end  
+
   context 'Ref("AWS::Region")' do
     let(:name) { 'Ref' }
     let(:input) { 'AWS::Region' }
